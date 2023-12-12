379fc7f2e9754728aa1f7b886a39469a3f0d5965
Johnathon Slightham <slighthamj@gmail.com>
2023-12-12 00:44:21 -0500

    Day 9

---
+++ 2023/day9/p2.rb
@@ -0,0 +1,11 @@
+inputs = []
+File.foreach('./2023/day9/data') do |line|
+  inputs << line.split(' ').map(&:to_i)
+end
+
+def add_derivative(inputs)
+  return 0 if inputs.all?(&:zero?)
+  inputs[-1] - add_derivative(inputs.map.with_index { |x, i| i.zero? ? x : inputs[i - 1] - x }[1..inputs.length])
+end
+
+puts inputs.map { |input| add_derivative input.reverse }.sum
